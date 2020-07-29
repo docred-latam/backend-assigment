ac9d054b18856f29f16be7dce15a08a594a7bb00
Rubén Padilla <rubbenpad2@gmail.com>
2020-07-29 02:16:26 -0500

    [ADD] docker settings to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "root",
+      password: "root",
+      database: "app",
+      entities: [],
+      synchronize: true
+    })
+  ],
+  controllers: []
 })
 export class AppModule {}
